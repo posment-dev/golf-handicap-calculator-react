105407421f82b6ee8c0517d7b5b0805219c2184f
Mirko R <mirko_richter@me.com>
2021-12-31 15:29:13 +0100

    feature: DB Server MongoDB added

---
+++ server/routes/course.js
@@ -0,0 +1,92 @@
+const express = require("express");
+
+// courseRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /course.
+const courseRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+
+// This section will help you get a list of all the courses.
+courseRoutes.route("/course").get(function (req, res) {
+  let db_connect = dbo.getDb("courses");
+  db_connect
+    .collection("courses")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single course by id
+courseRoutes.route("/course/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { id: req.params.id};
+  console.log('\n Get Course by ID: ' + req.params.id + "\n")
+  db_connect
+      .collection("courses")
+      .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+});
+
+// This section will help you create a new course.
+courseRoutes.route("/course/add").post(function (req, response) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  let db_connect = dbo.getDb();
+  let myobj = {
+    id: req.body.id,
+    name: req.body.name,
+    par: req.body.par,
+    slope: req.body.slope,
+    course_rating: req.body.cr,
+    tees: req.body.tees,
+  };
+  db_connect.collection("courses").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a course by id.
+courseRoutes.route("/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { id: req.params.id};
+  let newvalues = {
+    $set: {
+      id : req.body.id,
+      name: req.body.name,
+      par: req.body.par,
+      slope: req.body.slope,
+      course_rating: req.body.cr,
+      tees: req.body.tees,
+    },
+  };
+  db_connect
+    .collection("courses")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+// This section will help you delete a course
+courseRoutes.route("/courses/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect.collection("courses").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.status(obj);
+  });
+});
+
+module.exports = courseRoutes;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 8000;
+app.options('*', cors()); // include before other routes 
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+app.use(require("./routes/course"));
+// get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
